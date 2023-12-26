144bf91faa30628d5628d0a6ae80bc8dbcab72be
nam-web <65065658+nam-web@users.noreply.github.com>
2023-12-26 16:43:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ function firstWord(s) {
   // your code here
 	if(s==='' || !s.includes(' '))
 		return s;
-	const index = s.trim().indexOf(' ');
+	const trimstr = s.trim();
+	const index = trimstr.indexOf(' ');
 	return s.substring(0,index+1);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String:"); 
 alert(firstWord(s));
